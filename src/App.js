3f2fd8af49a79766a8ac071f394661f186ddb154
amariano-copperleaf <amariano@copperleaf.com>
2024-05-13 14:31:29 -0700

    initial commit

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Hi there! I will study a lot and earn skills that they will turn me to an asset! YEEAH!
         </p>
         <a
           className="App-link"
